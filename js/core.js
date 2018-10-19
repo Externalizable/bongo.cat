f3c84a034d2c3eca002a269665a57de1e5752185
Eric Huber <hey@erics.email>
2018-10-19 11:48:11 -0600

    Change "Keyboard" to "Piano", remove table.png, formatting

--- js/core.js
+++ js/core.js
@@ -103,30 +103,30 @@ $.play = function(instrument, key, state) {
     var commonKey = KeyEnum[key];
     var paw = (instrument == InstrumentEnum.BONGO ? key : key <= 5 && key != 0 ? 0 : 1);
     var id = (instrument == InstrumentEnum.MEOW ? "#mouth" : '#' + (paw == 0 ? "l" : "r") + 'paw');
-if (state == true) {
-    if (jQuery.inArray(commonKey, pressed) !== -1) {
-        return;
+    if (state == true) {
+        if (jQuery.inArray(commonKey, pressed) !== -1) {
+            return;
+        } else {
+            pressed.push(commonKey);
+        }
+        if (instrument != InstrumentEnum.MEOW) {
+            $(".instrument").each(function(index) {
+              if ($(this).attr('id') === instrumentName) {
+                $(this).css("visibility", "visible");
+              } else {
+                $(this).css("visibility", "hidden");
+              }
+            });
+        }
+        $.sound(instrumentName + key);
     } else {
-        pressed.push(commonKey);
+        pressed.remove(commonKey);
     }
-    if (instrument != InstrumentEnum.MEOW) {
-        $(".instrument").each(function(index) {
-          if ($(this).attr('id') === instrumentName) {
-            $(this).css("visibility", "visible");
-          } else {
-            $(this).css("visibility", "hidden");
-          }
-        });
+    if (instrument == InstrumentEnum.MEOW) {
+        $('#mouth').css("background-image", "url('images/m" + (state === true ? "2" : "1") + ".png')");
+    } else {
+        $(id).css("background-image", "url('images/" + (paw == 0 ? "l" : "r") + (state === true ? "2" : "1") + ".png')");
     }
-    $.sound(instrumentName + key);
-} else {
-    pressed.remove(commonKey);
-}
-if (instrument == InstrumentEnum.MEOW) {
-    $('#mouth').css("background-image", "url('images/m" + (state === true ? "2" : "1") + ".png')");
-} else {
-    $(id).css("background-image", "url('images/" + (paw == 0 ? "l" : "r") + (state === true ? "2" : "1") + ".png')");
-}
 }
 $.sound = function(filename) {
     lowLag.play(filename);

e29c496e763f872c80370039fde878320ec3c493
juanigp <juanigpisula@gmail.com>
2020-10-10 14:03:45 -0300

    removed console logs

--- js/core.js
+++ js/core.js
@@ -286,7 +286,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('../service-worker.js')
-      .then(reg => console.log('Service Worker: Registered (Pages)'))
-      .catch(err => console.log(`Service Worker: Error: ${err}`));
+      //.then(reg => console.log('Service Worker: Registered (Pages)'))
+      //.catch(err => console.log(`Service Worker: Error: ${err}`));
   });
 }
